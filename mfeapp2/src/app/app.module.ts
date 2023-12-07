fe64219123c6deff9e7595472ff09cf07da898f0
Shankhadeep <shankhadeepdhar1997@gmail.com>
2023-12-08 01:23:50 +0530

    CacheBusting commit

--- mfeapp2/src/app/app.module.ts
+++ mfeapp2/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { VersionCheckService } from './services/version-check.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component
+    Comp1Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [VersionCheckService],
   bootstrap: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
